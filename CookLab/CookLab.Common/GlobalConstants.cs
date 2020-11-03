9cb0bb4571cfac4e6b87bcd7515c85271e479320
Eva Doychinova <evadoychinova@gmail.com>
2020-11-03 14:54:37 +0200

    Create CookLab Project

---
+++ CookLab/CookLab.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace CookLab.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "CookLab";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
