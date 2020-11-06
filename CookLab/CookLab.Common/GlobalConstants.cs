b91ae5ccbc4747451fb9c523c05cd5b654871b07
Eva Doychinova <evadoychinova@gmail.com>
2020-11-06 11:56:34 +0200

    Import Bootstrap theme to HomePage

--- CookLab/CookLab.Common/GlobalConstants.cs
+++ CookLab/CookLab.Common/GlobalConstants.cs
@@ -5,5 +5,7 @@
         public const string SystemName = "CookLab";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string HomePageTitle = "Home Page";
     }
 }
