4d38d0ced96dbf607d6737e32d8c95cd3e2fb679
Eva Doychinova <evadoychinova@gmail.com>
2020-11-08 19:40:26 +0200

    Implement Categories Create and Listing

---
+++ CookLab/Models/CookLab.Models.ViewModels/Categories/CategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CookLab.Models.ViewModels.Categories
+{
+    using CookLab.Data.Models;
+    using CookLab.Services.Mapping;
+
+    public class CategoryViewModel : IMapFrom<Category>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ CookLab/Web/CookLab.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿namespace CookLab.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using CookLab.Models.InputModels.Categories;
+    using CookLab.Models.ViewModels.Categories;
+    using CookLab.Services.Data;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CategoriesController : BaseController
+    {
+        private readonly ICategoriesService categoriesService;
+
+        public CategoriesController(ICategoriesService categoriesService)
+        {
+            this.categoriesService = categoriesService;
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryInputModel inputModel)
+        {
+            await this.categoriesService.CreateAsync(inputModel.Name, inputModel.ImageUrl);
+
+            return this.Redirect("/Categories/All");
+        }
+
+        public IActionResult All()
+        {
+            var categories = this.categoriesService.GetAll<CategoryViewModel>();
+
+            var viewModel = new CategoriesListViewModel
+            {
+                Categories = categories,
+            };
+
+            return this.View(viewModel);
+        }
+    }
+}
